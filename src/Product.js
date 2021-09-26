6d58ba9c129ac763753001779a78944228eb9566
itika1 <itikaitzme@gmail.com>
2021-09-26 15:04:17 +0530

    deploy react-app

---
+++ src/Product.js
@@ -0,0 +1,11 @@
+import {Component} from 'react';
+class Product extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hi there</h1>
+            </div>
+        )
+    }
+}
+export default Product;
