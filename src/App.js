f00c80b35d6a5c6541db48c4e18a241752d9347a
itika1 <itikaitzme@gmail.com>
2021-09-28 19:54:06 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Title from './Title';
 import Product from './Product';
+import Mycomponent from './Mycomponent';
 function App() {
   return (
     <div className="App">
       <h1>LetsUpgrade</h1>
       <Title />
       <Product />
+      <Mycomponent />
     </div>
   );
 }
---
+++ src/Mycomponent.js
@@ -0,0 +1,11 @@
+import {Component} from 'react';
+class Mycomponent extends Component{
+    render(){
+        return(
+            <div>
+                <p>This is my first day Assignment. It's cool to working on this project.</p>
+            </div>
+        )
+    }
+}
+export default Mycomponent;
